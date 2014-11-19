4b3e42edff96f6a5960a5525dfd896157b8eecd9
Shan Hasan <shan119@hotmail.co.uk>
2014-11-19 15:48:07 +0000

    added users

--- lib/server.rb
+++ lib/server.rb
@@ -3,6 +3,7 @@ require 'data_mapper'
 
 require_relative '../lib/link.rb'
 require_relative '../lib/tag.rb'
+require_relative '../lib/user.rb'
 
 env = ENV["RACK_ENV"] || "development"
 
@@ -36,6 +37,19 @@ class MyApp < Sinatra::Base
     @links = tag ? tag.links : []
     erb :index
   end
+
+  get '/users/new' do
+    erb :"users/new"
+  end
+
+  post '/users' do
+    User.create(:email => params[:email],
+                :password => params[:password])
+    redirect to('/')
+  end
+
+
+
 
     # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 feature "User signs up" do
   scenario "when being logged out" do
-    expect{sign_up}.to change(user, :count).by(1)
+    expect{sign_up}.to change(User, :count).by(1)
     expect(page).to have_content("Welcome, alice@example.com")
     expect(User.first.email).to eq("alice@example.com")
   end
