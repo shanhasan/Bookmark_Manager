5c01673393da96025426faac36405dbf014fdef6
Shan Hasan <shan119@hotmail.co.uk>
2014-11-19 12:29:02 +0000

    updated server to include tags param

--- lib/server.rb
+++ lib/server.rb
@@ -24,7 +24,10 @@ class MyApp < Sinatra::Base
   post '/links' do
     url = params["url"]
     title = params["title"]
-    Link.create(:url => url, :title => title)
+    tags = params["tags"].split(" ").map do |tag|
+      Tag.first_or_create(:text => tag)
+    end
+    Link.create(:url => url, :title => title, :tags => tags)
     redirect to('/')
   end
 
