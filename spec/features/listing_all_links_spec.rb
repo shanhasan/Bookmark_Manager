35967f3ec132eae1e85c8affef6df970d7baff7d
Shan Hasan <shan119@hotmail.co.uk>
2014-11-19 11:30:20 +0000

    created test for add links

--- spec/features/listing_all_links_spec.rb
+++ spec/features/listing_all_links_spec.rb
@@ -9,7 +9,6 @@ feature "User browses the list of links" do
 
   scenario "when opening the homepage" do
     visit '/'
-    expect(page).to have_content('Current Links')
     expect(page).to have_content("Makers Academy")
   end
 end
