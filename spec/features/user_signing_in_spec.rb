082bac99f18d69d3feccd832fd1eec489ac5f127
Shan Hasan <shan119@hotmail.co.uk>
2014-11-20 15:49:29 +0000

    updated features folder, with more tests and modules

---
+++ spec/features/user_signing_in_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+require_relative 'helpers/session'
+
+include SessionHelpers
+
+feature "User signs in" do
+
+  before(:each) do
+    User.create(:email => 'test@test.com',
+                :password => 'test',
+                :password_confirmation => 'test')
+  end
+
+  scenario "with correct credentials" do
+    visit '/'
+    expect(page).not_to have_content("Welcome, test@test.com")
+    sign_in('test@test.com', 'test')
+    expect(page).to have_content("Welcome, test@test.com")
+  end
+
+  scenario "with incorrect credentials" do
+    visit '/'
+    expect(page).not_to have_content("Welcome, test@test.com")
+    sign_in('test@test.com', 'wrong')
+    expect(page).not_to have_content("Welcome, test@test.com")
+  end
+
+  #  def sign_in(email = "test@test.com",
+  #              password = "test")
+  #   visit '/sessions/new'
+  #   fill_in 'email', :with => email
+  #   fill_in 'password', :with => password
+  #   click_button 'Sign in'
+  # end
+
+end
