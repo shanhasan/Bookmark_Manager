2a476160e9e8531cd1c96cb28f3a3fb2d2911a8f
Shan Hasan <shan119@hotmail.co.uk>
2014-11-21 14:12:35 +0000

    added user forgot password spec

---
+++ spec/features/user_forgotten_password_spec.rb
@@ -0,0 +1,13 @@
+# require 'spec_helper'
+# require_relative 'helpers/session'
+
+# include SessionHelpers
+
+# feature "when forgetting your password" do
+
+#   scenario "when being logged in" do
+#     expect{sign_up}.to change(User, :count).by(1)
+#     expect(page).to have_content("Welcome, alice@example.com")
+#     expect(User.first.email).to eq("alice@example.com")
+#   end
+# end
