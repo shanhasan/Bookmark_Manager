3c752b1e9ccf5b324b10e332ab46f980410d77bf
Shan Hasan <shan119@hotmail.co.uk>
2014-11-21 14:08:25 +0000

    updated sessions/new

--- app/server.rb
+++ app/server.rb
@@ -30,67 +30,6 @@ class MyApp < Sinatra::Base
 
   helpers ApplicationHelpers
 
-#   get '/' do
-#     @links = Link.all
-#     erb :index
-#   end
-
-#   post '/links' do
-#     url = params["url"]
-#     title = params["title"]
-#     tags = params["tags"].split(" ").map{|tag| Tag.first_or_create(:text => tag)}
-#     Link.create(:url => url, :title => title, :tags => tags)
-#     redirect to('/')
-#   end
-
-#   get '/tags/:text' do
-#     tag = Tag.first(:text => params[:text])
-#     @links = tag ? tag.links : []
-#     erb :index
-#   end
-
-#   get '/users/new' do
-#     @user = User.new
-#     erb :"users/new"
-#   end
-
-#   post '/users' do
-#    @user = User.create(:email => params[:email],
-#                 :password => params[:password],
-#                 :password_confirmation => params[:password_confirmation])
-#     if @user.save
-#       session[:user_id] = @user.id
-#       redirect to('/')
-#     else
-#       flash.now[:errors] = @user.errors.full_messages
-#       erb :"users/new"
-#     end
-#   end
-
-#   get '/sessions/new' do
-#     erb :"sessions/new"
-#   end
-
-#   post '/sessions' do
-#   email, password = params[:email], params[:password]
-#   user = User.authenticate(email, password)
-#   if user
-#     session[:user_id] = user.id
-#     redirect to('/')
-#   else
-#     flash[:errors] = ["The email or password is incorrect"]
-#     erb :"sessions/new"
-#   end
-# end
-
-#   delete '/sessions' do
-#     flash[:notice] = "Good bye!"
-#     session[:user_id] = nil
-#     redirect to('/')
-# end
-
-    # start the server if ruby file executed directly
-  
   run! if app_file == $0
 end
  
