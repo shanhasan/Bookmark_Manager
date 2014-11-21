f27ed7464b238630ebbb37d5d7f978a9a339603b
Shan Hasan <shan119@hotmail.co.uk>
2014-11-21 14:07:56 +0000

    added controller users

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -17,4 +17,29 @@ class MyApp < Sinatra::Base
       erb :"users/new"
     end
   end
+
+  get '/users/forgot_password' do
+    erb :"users/forgot_password"
+  end
+
+  post '/users/forgot_password' do
+    user = User.first(email: params[:email]) 
+    user.password_token = (1..64).map{('A'..'Z').to_a.sample}.join
+    # user.password_token_timestamp = Time.now
+    user.save
+    'check your email'
+  end
+
+  get '/users/reset_password/:token' do
+    @token = params[:token]
+    user = User.first(password_token: @token)
+    erb :"users/new_password"
+  end
+
+  post '/users/reset_password' do
+    user = User.first(password_token: params[:password_token])
+    user.update(password: params[:password], password_confirmation: params[:password_confirmation], password_token: nil)
+    'pass update'
+  end
+
 end
