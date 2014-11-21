803ebfc61251d8e6aa318b92e9b11da7dca17104
Shan Hasan <shan119@hotmail.co.uk>
2014-11-21 14:10:21 +0000

    updated addinging_links spec

--- spec/features/adding_links_spec.rb
+++ spec/features/adding_links_spec.rb
@@ -29,8 +29,8 @@ feature "User adds a new link" do
 
   def add_link(url, title, tags =[])
     within('#new-link') do
-      fill_in 'url', :with => url
-      fill_in 'title', :with => title
+      fill_in 'url', with: url
+      fill_in 'title', with: title
       
       fill_in 'tags', :with => tags.join(' ')
       click_button 'Add Link'
