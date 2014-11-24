e9635fa4d87eac1029ff170880b1d8b302648f09
Shan Hasan <shan119@hotmail.co.uk>
2014-11-24 11:09:34 +0000

    updated users to include message notification, time stamp and password check

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -21,35 +21,46 @@ class MyApp < Sinatra::Base
   get '/users/forgot_password' do
     erb :"users/forgot_password"
   end
-
+  
   post '/users/forgot_password' do
     user = User.first(email: params[:email]) 
     user.password_token = (1..64).map{('A'..'Z').to_a.sample}.join
-    # user.password_token_timestamp = Time.now
+    user.password_token_timestamp = Time.now
     user.save
     send_simple_message(user.password_token)
     erb :"users/password_sent"
   end
 
-  def send_simple_message(password_token)
+  def send_simple_message(token)
     RestClient.post "https://api:key-3ax6xnjp29jd6fds4gc373sgvjxteol0"\
     "@api.mailgun.net/v2/samples.mailgun.org/messages",
     :from => "Password Reset Manager <me@samples.mailgun.org>",
     :to => "An excited user <#{params[:email]}>",
+    # :to => "Shan <shan119@hotmail.co.uk>",
     :subject => "Hello", 
-    :text => "Below is the link to rest your password: http://localhost:9292/users/reset_password/:password_token"
+    :text => "Below is the link to rest your password: http://localhost:9292/users/reset_password/#{token}"
   end
 
   get '/users/reset_password/:token' do
     @token = params[:token]
     user = User.first(password_token: @token)
-    erb :"users/new_password"
+    if Time.now < (user.password_token_timestamp + 3600)
+      erb :"users/new_password"
+    else
+      flash[:notice] = "Password token timed out!"
+      redirect to 'users/forgot_password'
+    end
   end
 
   post '/users/reset_password' do
-    user = User.first(password_token: params[:password_token])
-    user.update(password: params[:password], password_confirmation: params[:password_confirmation], password_token: nil)
-    'pass update'
+    user = User.first(password_token: params[:password_token]) 
+    if user.update(password: params[:password], password_confirmation: params[:password_confirmation], password_token: nil)
+      session[:user_id] = user.id
+      redirect to('/')
+    else
+      flash.now[:errors] = user.errors.full_messages
+      erb :"users/new_password"
+    end
   end
 
 end
